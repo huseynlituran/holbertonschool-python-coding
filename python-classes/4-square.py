0a72f2f369b35bcea3af7c98c3719c6d11726dc6
huseynlituran <huseynlituran17@gmail.com>
2025-11-21 15:22:21 +0000

    square with myprint

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Square class with size property, area and print methods."""
+
+
+class Square:
+    """Square class with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initialize square with size using the size setter."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with type and value validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square with the character #."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
